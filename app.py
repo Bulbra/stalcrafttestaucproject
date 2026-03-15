d237e4a42a164711e5f0c55f3b6e414f055dfbce
MaxBaev <mbaev2012@mail.ru>
2026-03-15 11:44:28 +0300

    added .env file removed settings file

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import requests
 from pprint import pprint, pp
-from settings import app_token, user_token
+from dotenv import load_dotenv
+import os
 
+# env
+load_dotenv()
+app_token = os.getenv("app_token")
+
+# vars
 item_id = "zzjgn"
-response = requests.get(headers={"Authorization": app_token},
-    url=f"https://dapi.stalcraft.net/ru/auction/{item_id}/history")
+region = "ru"
 
+# response
+response = requests.get(headers={"Authorization": app_token},
+    url=f"https://dapi.stalcraft.net/{region}/auction/{item_id}/history")
+
+# print results
 pp(response.json())
