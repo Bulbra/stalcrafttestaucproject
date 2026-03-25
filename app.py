c01675667eec3d16eabeff8093333401ea271e4c
Bulbra <darthartem76@gmail.com>
2026-03-25 17:09:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 from stalcraftapi_model import Stalcraft
 from db import crud
 
-# env
+# envarement
 load_dotenv()
 app_token = os.getenv("token")
 secret_token = os.getenv("secret_token")
