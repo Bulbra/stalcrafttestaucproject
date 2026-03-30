c2eec696491f85b56ca50290bcac8938df4b7ebf
Bulbra <darthartem76@gmail.com>
2026-03-30 16:26:56 +0300

    Fix crud, functions -> async functions

--- db/crud.py
+++ db/crud.py
@@ -2,17 +2,19 @@ import datetime
 from sqlalchemy.orm import Session
 from db.engine import engine
 from db.models import ItemHistoryLot
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 async def add_history_lot_async(
         amount: int, time: datetime.datetime,
         price: int) -> None:
-    with Session(bind=engine) as session:
+    with await AsyncSession(bind=engine) as session:
         session.add(ItemHistoryLot(
             amount=amount, time=time, price=price))
-        session.commit()
+        await session.commit()
 
 async def get_all_history_async():
-    with Session(bind=engine) as session:
+    with await AsyncSession(bind=engine) as session:
         stmt = select(ItemHistoryLot)
         result = session.execute(stmt)
         history: ItemHistoryLot = result.scalar()
@@ -21,7 +23,7 @@ async def get_all_history_async():
         return history
             
 async def gey_history_by_time_async(time: datetime.datetime):
-    with Session(bind=engine) as session:
+    with await AsyncSession(bind=engine) as session:
         stmt = select(ItemHistoryLot).where(ItemHistoryLot.time == time)
         result = session.execute(stmt)
         history: ItemHistoryLot = result.scalar()
@@ -30,7 +32,7 @@ async def gey_history_by_time_async(time: datetime.datetime):
         return history
             
 async def gey_history_by_amount_async(amount: int):
-    with Session(bind=engine) as session:
+    with await AsyncSession(bind=engine) as session:
         stmt = select(ItemHistoryLot).where(ItemHistoryLot.amount == amount)
         result = session.execute(stmt)
         history: ItemHistoryLot = result.scalar()
